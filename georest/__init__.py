7721ac3869d09084edb93a4667162b6eaebea1d6
Panu Lahtinen <pnuu+git@iki.fi>
2023-06-13 13:34:19 +0300

    Update to version 0.8.1

--- georest/__init__.py
+++ georest/__init__.py
@@ -22,7 +22,7 @@ from geoserver.support import DimensionInfo
 
 from georest import utils
 
-__version__ = "0.8.0"
+__version__ = "0.8.1"
 
 # These layer attributes can be set
 LAYER_ATTRIBUTES = ["title", "abstract", "keywords"]
